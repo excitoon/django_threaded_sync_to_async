43a6a5a6e5ebf6a4e409715645546dd79185e068
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-05-28 08:44:00 +0300

    Removed redundant closure in test.

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -10,11 +10,10 @@ import django_threaded_sync_to_async
 
 class TestExecutor(unittest.IsolatedAsyncioTestCase):
     async def testConcurrent(self):
-        cv = threading.Condition()
         workers = 50
         timeout = 0.1
 
-        def long_call(threads):
+        def long_call(cv, threads):
             threads.add(threading.current_thread().name)
 
             def notify_cv():
@@ -28,8 +27,8 @@ class TestExecutor(unittest.IsolatedAsyncioTestCase):
                 return cv.wait_for(lambda: len(threads) == workers, timeout)
 
         @asgiref.sync.sync_to_async
-        def decorated_long_call(threads):
-            return long_call(threads)
+        def decorated_long_call(*args):
+            return long_call(*args)
 
         @contextlib.asynccontextmanager
         async def empty(**kwargs):
@@ -38,9 +37,10 @@ class TestExecutor(unittest.IsolatedAsyncioTestCase):
         for parallel, context in ((False, empty), (True, django_threaded_sync_to_async.Executor)):
             for decorated, function in ((False, asgiref.sync.sync_to_async(long_call)), (True, decorated_long_call)):
                 with self.subTest(parallel=parallel, decorated=decorated):
+                    cv = threading.Condition()
                     threads = set()
                     async with context(max_workers=workers):
-                        tt = [asyncio.create_task(function(threads)) for _ in range(workers)]
+                        tt = [asyncio.create_task(function(cv, threads)) for _ in range(workers)]
                         try:
                             for c in asyncio.as_completed(tt, timeout=timeout):
                                 self.assertEqual(await c, parallel)
