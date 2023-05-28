41329bb25438ee151951167627e7bb73f97017cd
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-05-28 22:06:28 +0300

    Version 1.0.2.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(f"{os.path.dirname(os.path.abspath(__file__))}/requirements.txt") as r
     with open(f"{os.path.dirname(os.path.abspath(__file__))}/README.md") as readme:
         setuptools.setup(
             name="django_threaded_sync_to_async",
-            version="1.0.1",
+            version="1.0.2",
             description="Tools for replacing `sync_to_async` calls to `sync_to_async(thread_sensitive=False, executor=...)`, effectively allowing Django to make calls to database concurrently",
             long_description=readme.read(),
             long_description_content_type="text/markdown",
